fb7bf1b92ad2f428c401c52aed83045d2858fb4e
Ismael-r-pithan <pithanismael@gmail.com>
2022-02-07 22:20:37 -0300

    criando repositorio

--- src/main/java/com/parkingcontrol/api/repositories/ParkingSpotRepository.java
+++ src/main/java/com/parkingcontrol/api/repositories/ParkingSpotRepository.java
@@ -1,5 +1,13 @@
 package com.parkingcontrol.api.repositories;
 
-public interface ParkingSpotRepository {
+import java.util.UUID;
+
+import com.parkingcontrol.api.model.ParkingSpotModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID>{
     
 }
