49abe2e33541c03be283a21f4919061e9c45b0ad
Ismael-r-pithan <pithanismael@gmail.com>
2022-02-07 23:52:14 -0300

    adicionando recurso delete

--- src/main/java/com/parkingcontrol/api/controller/ParkingSpotController.java
+++ src/main/java/com/parkingcontrol/api/controller/ParkingSpotController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,5 +74,19 @@ public class ParkingSpotController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotModelOptional.get());
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteParkingSpot (@PathVariable(value = "id") UUID id) {
+
+        Optional<ParkingSpotModel> parkingSpotModelOptional = parkingSpotService.findById(id);
+
+        if (!parkingSpotModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking spot not foud");
+        }
+
+        parkingSpotService.delete(parkingSpotModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Parking Spot deleted successfully");
     }
 }
